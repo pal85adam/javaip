0c9db3c6be3c27c1e1c2afc0ae637c474a74dbcd
Mohammed Nijim <pal.85.adam@gmail.com>
2020-03-19 16:17:21 +0100

    Updating Main class

--- Main.java
+++ Main.java
@@ -1,18 +1,49 @@
 import java.util.ArrayList;
 
 public class Main{
-    ArrayList<Task> tasks = new ArrayList<>();
+    ArrayList<Task> tasks;
+
+    public Main(){
+        tasks = new ArrayList<>();
+    }
+
+    public String getMainMenu(){
+        return ">> Welcome to ToDoLy \n"
+        + ">> You have X tasks todo and Y tasks are done! \n"
+        + ">> Pick an option: \n"
+        + ">> (1) Show Task List (by date or project) \n"
+        + ">> (2) Add New Task \n"
+        + ">> (3) Edit Task (update, mark as done, remove) \n"
+        + ">> (4) Save and Quit \n"
+        + ">> \n";
+    }
+
+    public String getTaskList(){
+        return "";
+    }
+
+    public void addTasks(){
+        
+    }
+
+    public void updateTask(){
+        
+    }
+
+    public void doneTask(){
+        
+    }
+
+    public void removeTask(){
+        
+    }
+
+    public void quitAndSave(){
+        
+    }
+
     public static void main(String[] args){
 
     }
-
-    // ">> Welcome to ToDoLy \n"
-    // ">> You have X tasks todo and Y tasks are done! \n"
-    // ">> Pick an option: \n"
-    // ">> (1) Show Task List (by date or project) \n"
-    // ">> (2) Add New Task \n"
-    // ">> (3) Edit Task (update, mark as done, remove) \n"
-    // ">> (4) Save and Quit \n"
-    // ">> \n"
 
 }
